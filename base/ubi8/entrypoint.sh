8000e81777bbe4e8cac6e71e5a87bc14e6239096
ThanhPham <amunthestormer@gmail.com>
2024-09-04 15:57:11 +0700

    update

--- base/ubi8/entrypoint.sh
+++ base/ubi8/entrypoint.sh
@@ -2,4 +2,24 @@
 
 source kubedock_setup
 
+set -e
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+if ! grep -Fq "${USER_ID}" /etc/passwd; then
+    # current user is an arbitrary 
+    # user (its uid is not in the 
+    # container /etc/passwd). Let's fix that    
+    sed -e "s/\${USER_ID}/${USER_ID}/g" \
+        -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
+        -e "s/\${HOME}/\/home\/user/g" \
+	/.passwd.template > /etc/passwd
+    
+    sed -e "s/\${USER_ID}/${USER_ID}/g" \
+        -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
+        -e "s/\${HOME}/\/home\/user/g" \
+	/.group.template > /etc/group
+fi
+
 exec "$@"
