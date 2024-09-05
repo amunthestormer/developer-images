9d7b7b31a3c0e5707d3c811304c43e04ed211ec8
ThanhPham <amunthestormer@gmail.com>
2024-09-05 08:07:38 +0700

    update

--- base/ubi8/entrypoint.sh
+++ base/ubi8/entrypoint.sh
@@ -10,16 +10,19 @@ export GROUP_ID=$(id -g)
 if ! grep -Fq "${USER_ID}" /etc/passwd; then
     # current user is an arbitrary 
     # user (its uid is not in the 
-    # container /etc/passwd). Let's fix that    
-    sed -e "s/\${USER_ID}/${USER_ID}/g" \
-        -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
-        -e "s/\${HOME}/\/home\/user/g" \
-	/.passwd.template > /etc/passwd
+    # container /etc/passwd). Let's fix that 
+
+    # sed -e "s/\${USER_ID}/${USER_ID}/g" \
+    # -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
+    # -e "s/\${HOME}/\/home\/user/g" \
+    # /.passwd.template > /etc/passwd   
+    echo "user:x:${USER_ID}:${GROUP_ID}:user:${HOME}:/bin/bash" > /etc/passwd
     
-    sed -e "s/\${USER_ID}/${USER_ID}/g" \
-        -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
-        -e "s/\${HOME}/\/home\/user/g" \
-	/.group.template > /etc/group
+    # sed -e "s/\${USER_ID}/${USER_ID}/g" \
+    #     -e "s/\${GROUP_ID}/${GROUP_ID}/g" \
+    #     -e "s/\${HOME}/\/home\/user/g" \
+	# /.group.template > /etc/group
+    echo "user:x:${USER_ID}:${GROUP_ID}" > /etc/group
 fi
 
 exec "$@"
